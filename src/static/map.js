0994f4037c6eb91853276d8d5cdf2db1dd9cbfa5
Jan Wirth <jottenwe@gmail.com>
2017-12-10 20:43:20 +0100

    Refactor

--- src/static/map.js
+++ src/static/map.js
@@ -1,16 +1,30 @@
 import GoogleMapsLoader from 'google-maps'
 GoogleMapsLoader.KEY = 'AIzaSyB0S7OJbqD7Oth6aJnY7TFIyNZspaSjOD4'
 
+// A registry where we keep all maps that were already initialized
+// because we reuse them in bindNextMapsInstance
+// https://stackoverflow.com/questions/10485582/what-is-the-proper-way-to-destroy-a-map-instance
 const mapRegister = [
 ]
 
-const bindFreeMapsInstance = google => el => {
+// 'global' library placeholder
+var google
+
+// fill the placeholder
+// loadedGoogle : Promise Google
+const loadGoogle = new Promise( ( resolve, reject ) => {
+  GoogleMapsLoader.load( loadedGoogle => {
+    google = loadedGoogle
+    resolve(google)
+  })
+})
+
+const bindNextMapsInstance = el => {
   const freeInstance = mapRegister.find( entry => {
     return !entry.getDiv().parentElement
   })
 
   if (freeInstance) {
-    console.log('replacing')
     el.parentElement.replaceChild(freeInstance.getDiv(), el)
     google.maps.event.trigger(freeInstance, "resize")
   } else {
@@ -26,40 +40,42 @@ const mapOptions ={
   zoom: 8
 }
 
-const makeMap = el => {
-  GoogleMapsLoader.load( google => {
-    bindFreeMapsInstance(google)(el)
-  })
+// makeMap : Node -> Effect node
+const makeMap = el => loadGoogle.then( () => bindNextMapsInstance(el) )
+
+// watchNewNodesAndBind : Node -> Effect Node
+const watchNewNodesAndBind = rootEl => {
+  // prepare observer
+  const observer = new MutationObserver(function(mutations) {
+    mutations.map( mutation => {
+      Array.from(mutation.addedNodes).map( node => {
+        node.tagName === 'GMAP' && !node.firstChild
+          ? makeMap(node)
+          : 'noop'
+
+      })
+      Array.from(mutation.removedNodes).map( node => {
+      })
+    })
+
+  });
+  // configure observer
+  const subtree = {
+    attributes: false,
+    childList: true,
+    subtree: true,
+    characterData: false
+  };
+  // Start the actual observation
+  observer.observe( rootEl, subtree )
 }
 
-// create an observer instance
-var observer = new MutationObserver(function(mutations) {
-  mutations.map( mutation => {
-    Array.from(mutation.addedNodes).map( node => {
-      node.tagName === 'GMAP' && !node.firstChild
-        ? makeMap(node)
-        : 'noop'
-      node.tagName === 'GMAP'
-        ? console.log(node)
-        : 'noop'
-    })
-    Array.from(mutation.removedNodes).map( node => {
-    })
-  })
-
-});
- 
-// configuration of the observer:
-var config = {
-  attributes: false,
-  childList: true,
-  subtree: true,
-  characterData: false
-};
-
+// init : Node -> Effect Node
 const init = rootEl=> {
+  // Observe nodes that are already in place
   Array.from(rootEl.querySelectorAll('gmap')).map( mapEl => makeMap(mapEl))
-  observer.observe( rootEl, config )
+  // Observe new nodes that come in from outer space
+  watchNewNodesAndBind(rootEl)
 }
 
 
